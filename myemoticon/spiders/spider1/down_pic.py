f5624c8f5d3c3e052e533782649210ebc87f03af
GHLisz <leeshzh@outlook.com>
2017-07-28 22:44:45 +0800

    spider finished, basically

--- myemoticon/spiders/spider1/down_pic.py
+++ myemoticon/spiders/spider1/down_pic.py
@@ -1,17 +1,60 @@
-from multiprocessing import Pool
+import os
+import sys
+import urllib.request
+import socket
 from time import sleep
+from multiprocessing.pool import ThreadPool
+from emoticon.models import Photo
+from spiders.configs import pic_root_folder
+
+socket.setdefaulttimeout(300)
 
 
-def job(url):
-    sleep(3)
-    print(url)
+def save_web_pic_to(pic_url, pic_path):
+    print('Downloading: ' + pic_url)
+    try:
+        urllib.request.urlretrieve(pic_url, pic_path)
+        return True
+    except:
+        sleep(120)
+        print(sys.exc_info())
+        return False
 
 
-def job_multi():
-    pool = Pool(100)
-    urls = range(100)
-    pool.map(job, urls)
+def save_pic_with_bk(pic_url, pic_path, bk_pic_url=None):
+    pic_folder = os.path.dirname(pic_path)
+    os.makedirs(pic_folder, exist_ok=True)
+    if os.path.exists(pic_path):
+        return
+
+    r = save_web_pic_to(pic_url, pic_path)
+    if (not r) and bk_pic_url:
+        save_web_pic_to(bk_pic_url, pic_path)
+
+
+def get_pic_args():
+    photos = Photo.objects.filter(cached=False)
+    for photo in photos:
+        pic_url = photo.old_url if photo.old_url.startswith('http:') else 'http:' + photo.old_url
+        bk_pic_url = photo.bk_url if photo.bk_url.startswith('http:') else 'http:' + photo.bk_url
+        pic_path = pic_root_folder + '/' + \
+                   '/'.join(bk_pic_url.
+                            replace('http://', '').
+                            split('/')[1:])
+        yield (pic_url, pic_path, bk_pic_url)
+
+
+def save_pic_wrapper(args):
+    print(args)
+    return save_pic_with_bk(*args)
+
+
+def save_pic_job_multi():
+    pool = ThreadPool(4)
+    pool.map(save_pic_wrapper, get_pic_args())
 
 
 if __name__ == '__main__':
-    job_multi()
+    for i in get_pic_args():
+        print(i)
+        print(type(i))
--- myemoticon/spiders/spider1/web_index.py
+++ myemoticon/spiders/spider1/web_index.py
@@ -1,8 +1,6 @@
 from .web_photo import WebPhoto
 from .web_article import WebArticle
 from spiders.utils import get_soup_from_url
-from spiders.utils import error_list_logger
-from emoticon.models import Photo, Article
 
 
 def get_article_ids_in_page(page_num):
@@ -13,8 +11,16 @@ def get_article_ids_in_page(page_num):
     return article_ids
 
 
-def get_max_page_num():
-    soup = get_soup_from_url('article/list/')
+def get_standalone_photo_ids_in_page(page_num):
+    soup = get_soup_from_url('photo/list/?page={}'.format(page_num))
+    photo_ids = []
+    for i in soup.select('a.col-xs-6.col-sm-3'):
+        photo_ids.append(i['href'].split('/')[-1])
+    return photo_ids
+
+
+def get_max_page_num(url='article/list/'):
+    soup = get_soup_from_url(url)
     return int(soup.select('ul.pagination > li > a')[-2].get_text())
 
 
@@ -57,4 +63,34 @@ def update_articles_incremental():
 
         if at_least_one_article_in_db:
             print('This page contains cached articles, skip the rest pages')
+            break
+
+
+def update_standalone_photos():
+    max_page_num = get_max_page_num('photo/list/')
+    print(max_page_num)
+    for i in range(max_page_num, 0, -1):
+        photo_ids = get_standalone_photo_ids_in_page(i)
+        for photo_id in photo_ids:
+            photo = WebPhoto(old_id=photo_id)
+            photo.save()
+
+
+def update_standalone_photos_incremental():
+    max_page_num = get_max_page_num()
+
+    for i in range(1, max_page_num+1):
+        photo_ids = get_standalone_photo_ids_in_page(i)
+
+        at_least_one_photo_in_db = False
+
+        for photo_id in photo_ids:
+            photo = WebPhoto(old_id=photo_id)
+            r = photo.save()
+
+            if not r:
+                at_least_one_photo_in_db = True
+
+        if at_least_one_photo_in_db:
+            print('This page contains cached photos, skip the rest pages')
             break
