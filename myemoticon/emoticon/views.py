27ea12e53a44a153bacbbd8bfa8fbd768b462d4b
GHLisz <leeshzh@outlook.com>
2017-07-30 22:19:10 +0800

    add view class

--- myemoticon/emoticon/views.py
+++ myemoticon/emoticon/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article, Photo
+
 
 # Create your views here.
+class ArticleListView(ListView):
+    queryset = Article.objects.all()
+    context_object_name = 'articles'
+    paginate_by = 10
+    template_name = 'emoticon/article_list.html'
+
+
+class StandalonePhotoListView(ListView):
+    queryset = Photo.objects.filter(article_id__isnull=True)
+    context_object_name = 'standalone_photos'
+    paginate_by = 50
+    template_name = ''
