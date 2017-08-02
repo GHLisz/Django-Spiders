fa4cf40183063c99f29d07a96615f1643ac924b8
GHLisz <leeshzh@outlook.com>
2017-08-02 22:35:36 +0800

    update templates using bs

--- myemoticon/emoticon/views.py
+++ myemoticon/emoticon/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Article, Photo
 
 
@@ -16,3 +17,20 @@ class StandalonePhotoListView(ListView):
     context_object_name = 'standalone_photos'
     paginate_by = 50
     template_name = ''
+
+
+def article_list(request):
+    object_list = Article.objects.all()
+    paginator = Paginator(object_list, 10)
+    page = request.GET.get('page')
+
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+
+    return render(request, 'emoticon/article_list.html', {
+        'articles': articles,
+    })
